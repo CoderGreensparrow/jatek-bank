7fb451a51558cda05c74d116d8f0169edadfcc71
Varga Donat <vdonatj2@gmail.com>
2021-09-16 22:21:55 +0200

    CSS kezdet, mobilkész navigációs sáv

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
 document.write(`<nav>
 <a href="/jatek-bank">Főoldal</a>
 <a href="/jatek-bank/Q&A.html">Kérdések és válaszok</a>
-<a href="/jatek-bank/Info.html">További információk</a>`)
+<a href="/jatek-bank/Info.html">További információk</a></nav>`)
